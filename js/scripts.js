5a0c6412788bedd1e03a660128896c1fb2b4fd0b
Nikolay Antonovich <TesaloNick@gmail.com>
2022-04-07 00:29:35 +0300

    add experience-block without border gradient

--- js/scripts.js
+++ js/scripts.js
@@ -134,28 +134,28 @@
 
 // //-----------------------возрастающие числа------------------------------
 
-// function countup(className) {
-//   var countBlockTop = $("." + className).offset().top;
-//   var windowHeight = window.innerHeight;
-//   var show = true;
+function countup(className) {
+  var countBlockTop = $("." + className).offset().top;
+  var windowHeight = window.innerHeight;
+  var show = true;
 
-//   $(window).scroll(function () {
-//     if (show && (countBlockTop < $(window).scrollTop() + windowHeight)) {
-//       show = false;
+  $(window).scroll(function () {
+    if (show && (countBlockTop < $(window).scrollTop() + windowHeight)) {
+      show = false;
 
-//       $('.' + className).spincrement({
-//         from: 1,
-//         duration: 4000,
-//       });
-//     }
-//   })
-// }
+      $('.' + className).spincrement({
+        from: 1,
+        duration: 4000,
+      });
+    }
+  })
+}
 
-// $(function () {
-//   countup("count", $(".count").text());
-//   countup("count2", $(".count2").text());
-//   countup("count3", $(".count3").text());
-// });
+$(function () {
+  countup("count", $(".count").text());
+  countup("count2", $(".count2").text());
+  countup("count3", $(".count3").text());
+});
 
 // //------------------------------всплывающие блоки-------------------------
 
