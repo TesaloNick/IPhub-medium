f34c292fb459e8cd7c4d14917d35278c5baf5dd6
Nikolay Antonovich <TesaloNick@gmail.com>
2022-11-22 15:04:10 +0300

    fix sliders, service slider hover, pop-up

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,3 @@
-// //--------------------выбор языка-------------------------
-
-// var ddData = [
-//   {
-//     text: "EN",
-//     value: 1,
-//     selected: true,
-//     imageSrc: "../img/header/flags/gb.svg"
-//   },
-//   {
-//     text: "AD",
-//     value: 2,
-//     selected: false,
-//     imageSrc: "../img/header/flags/ad.svg"
-//   },
-//   {
-//     text: "RU",
-//     value: 3,
-//     selected: false,
-//     imageSrc: "../img/header/flags/ru.svg"
-//   },
-//   {
-//     text: "BY",
-//     value: 4,
-//     selected: false,
-//     imageSrc: "../img/header/flags/by.svg"
-//   },
-//   {
-//     text: "IS",
-//     value: 5,
-//     selected: false,
-//     imageSrc: "../img/header/flags/yiddish.svg"
-//   }
-// ];
-
-// --------------------слайдеры-------------------------
-
-// $('#lan').ddslick({
-//   data: ddData,
-//   defaultSelectedIndex: 2,
-// });
-
 $('.services__slider').slick({
   infinite: false,
   arrows: true,
@@ -48,9 +6,6 @@ $('.services__slider').slick({
   swipeToSlide: true,
   prevArrow: "<div class='prev'><img src='../img/main/button-arrow-right.svg' class='prev-arrow'></div>",
   nextArrow: "<div class='next'><img src='../img/main/button-arrow-right.svg' class='next-arrow'></div>",
-  // variableWidth: true,
-  // dots: true,
-  // centerMode: true,
   responsive: [
     {
       breakpoint: 1500,
@@ -169,128 +124,11 @@ let amountOfSlidesCases = document.querySelectorAll('.case').length;
 let amountOfSlidesTeam = document.querySelectorAll('.member').length;
 let windowWidth = $(window).width();
 
-if (windowWidth <= 1500 && windowWidth > 1256) {
+if (windowWidth <= 1255) {
   $('.services__slider').slick('slickRemove', amountOfSlidesServices - 1);
-} else if (windowWidth <= 1256 && windowWidth > 1000) {
-  $('.services__slider').slick('slickRemove', amountOfSlidesServices - 1);
-  $('.services__slider').slick('slickRemove', amountOfSlidesServices - 2);
-  $('.team__slider').slick('slickRemove', amountOfSlidesTeam - 1);
-  $('.team__slider').slick('slickRemove', amountOfSlidesTeam - 2);
   $('.cases__slider').slick('slickRemove', amountOfSlidesCases - 1);
-} else if (windowWidth <= 1000 && windowWidth > 900) {
-  $('.services__slider').slick('slickRemove', amountOfSlidesServices - 1);
-  $('.services__slider').slick('slickRemove', amountOfSlidesServices - 2);
-  $('.services__slider').slick('slickRemove', amountOfSlidesServices - 3);
   $('.team__slider').slick('slickRemove', amountOfSlidesTeam - 1);
-  $('.team__slider').slick('slickRemove', amountOfSlidesTeam - 2);
-  $('.team__slider').slick('slickRemove', amountOfSlidesTeam - 3);
-  $('.cases__slider').slick('slickRemove', amountOfSlidesCases - 1);
-} else if (windowWidth <= 900 && windowWidth > 700) {
-  $('.services__slider').slick('slickRemove', amountOfSlidesServices - 1);
-  $('.services__slider').slick('slickRemove', amountOfSlidesServices - 2);
-  $('.services__slider').slick('slickRemove', amountOfSlidesServices - 3);
-  $('.team__slider').slick('slickRemove', amountOfSlidesTeam - 1);
-  $('.team__slider').slick('slickRemove', amountOfSlidesTeam - 2);
-  $('.team__slider').slick('slickRemove', amountOfSlidesTeam - 3);
-  $('.cases__slider').slick('slickRemove', amountOfSlidesCases - 1);
-  $('.cases__slider').slick('slickRemove', amountOfSlidesCases - 2);
-} else if (windowWidth <= 700 && windowWidth > 450) {
-  $('.services__slider').slick('slickRemove', amountOfSlidesServices - 1);
-  $('.services__slider').slick('slickRemove', amountOfSlidesServices - 2);
-  $('.services__slider').slick('slickRemove', amountOfSlidesServices - 3);
-  $('.services__slider').slick('slickRemove', amountOfSlidesServices - 4);
-  $('.team__slider').slick('slickRemove', amountOfSlidesTeam - 1);
-  $('.team__slider').slick('slickRemove', amountOfSlidesTeam - 2);
-  $('.team__slider').slick('slickRemove', amountOfSlidesTeam - 3);
-  $('.team__slider').slick('slickRemove', amountOfSlidesTeam - 4);
-  $('.cases__slider').slick('slickRemove', amountOfSlidesCases - 1);
-  $('.cases__slider').slick('slickRemove', amountOfSlidesCases - 2);
-} else if (windowWidth <= 450) {
-  $('.services__slider').slick('slickRemove', amountOfSlidesServices - 1);
-  $('.services__slider').slick('slickRemove', amountOfSlidesServices - 2);
-  $('.services__slider').slick('slickRemove', amountOfSlidesServices - 3);
-  $('.services__slider').slick('slickRemove', amountOfSlidesServices - 4);
-  $('.services__slider').slick('slickRemove', amountOfSlidesServices - 5);
-  $('.team__slider').slick('slickRemove', amountOfSlidesTeam - 1);
-  $('.team__slider').slick('slickRemove', amountOfSlidesTeam - 2);
-  $('.team__slider').slick('slickRemove', amountOfSlidesTeam - 3);
-  $('.team__slider').slick('slickRemove', amountOfSlidesTeam - 4);
-  $('.team__slider').slick('slickRemove', amountOfSlidesTeam - 5);
-  $('.cases__slider').slick('slickRemove', amountOfSlidesCases - 1);
-  $('.cases__slider').slick('slickRemove', amountOfSlidesCases - 2);
 }
-// $('.service').on('click', function () {
-// console.log(this);
-// $('.services__slider').slick('slickRemove', $('.slick-slide').index(this) - 1)
-// });
-
-// if ($(window).width() <= 900) {
-//   $('.steps__content-block').slick({
-//     infinite: true,
-//     arrows: false,
-//     slidesToShow: 1,
-//     dots: true,
-//   });
-
-//   // let paddingTopDots = 0;
-//   // paddingTopDots = document.querySelector(".order__steps").offsetHeight;
-//   // document.querySelector('.steps__content-block .slick-dots').style = `top: -${paddingTopDots + 50}px`;
-
-//   // if ($(window).width() <= 450) {
-//   //   document.querySelector('.steps__content-block .slick-dots').style = `top: -${paddingTopDots + 30}px`;
-//   // };
-// }
-
-// $('.comments__slider').slick({
-//   infinite: true,
-//   arrows: false,
-//   slidesToShow: 3,
-//   variableWidth: true,
-//   dots: true,
-//   responsive: [
-//     {
-//       breakpoint: 1200,
-//       settings: {
-//         slidesToShow: 2,
-//         variableWidth: false,
-//       }
-//     },
-//     {
-//       breakpoint: 767,
-//       settings: {
-//         slidesToShow: 1,
-//         variableWidth: false,
-//       }
-//     }
-//   ]
-// });
-
-// //--------------------определение цены-------------------------
-
-// const arrowLeft = document.querySelector('.input__arrow-left');
-// const arrowRight = document.querySelector('.input__arrow-right');
-// const number = document.querySelector('.sessions__input');
-// const multiplyNumber = document.querySelector('.input__multiply-number');
-// const sessionsForm = document.querySelector('.sessions__form');
-
-// arrowRight.addEventListener('click', () => {
-//   number.value++
-//   multiplyNumber.innerHTML = +number.value * 49;
-// })
-
-// arrowLeft.addEventListener('click', () => {
-//   if (+number.value > 1) number.value--
-//   multiplyNumber.innerHTML = +number.value * 49;
-// })
-
-// number.addEventListener('change', () => {
-//   multiplyNumber.innerHTML = +number.value * 49;
-// })
-
-// sessionsForm.addEventListener('submit', (e) => {
-//   e.preventDefault()
-//   multiplyNumber.innerHTML = +number.value * 49;
-// })
 
 //-----------------------возрастающие числа------------------------------
 
@@ -313,45 +151,13 @@ function countup(className) {
 
 $(function () {
   countup("count", $(".count").text());
-  // countup("count2", $(".count2").text());
-  // countup("count3", $(".count3").text());
 });
 
-// //------------------------------всплывающие блоки-------------------------
 
-// function appearup(classStart, classAppear) {
-//   var show = true;
-
-//   $(window).scroll(function () {
-//     var distanceTop = $(classStart).offset().top;
-//     if (show && ($(window).scrollTop() + window.innerHeight > distanceTop)) {
-//       show = false;
-//       $(classAppear).addClass('animate');
-//     }
-//   });
-// }
-
-// $(function () {
-//   appearup(".help__title", ".help");
-//   appearup(".second-video-block", ".second-app");
-//   appearup(".workers", ".workers");
-//   appearup(".advantages", ".advantages");
-//   appearup(".selection__title", ".selection");
-//   appearup(".about-service", ".about-service");
-//   appearup(".sets", ".sets");
-//   appearup(".order__title", ".order");
-//   appearup(".comments", ".comments");
-//   appearup(".service__title", ".service");
-//   appearup(".child-help__title", ".child-help");
-//   appearup(".footer__left-block", ".footer");
-// });
-
-//------------------------------меню-------------------------
 
 function toggleMenu() {
   $('.header__burger-menu').toggleClass('active-menu')
   $('.header__nav').toggleClass('active-menu')
-  // $('.open-menu-scroll').toggleClass('active-menu')
 }
 
 $(function () {
@@ -402,28 +208,9 @@ document.querySelectorAll('.pop-up__textarea').forEach(item => {
 function removeMenu() {
   $('.header__burger-menu').removeClass('active-menu')
   $('.header__nav').removeClass('active-menu')
-  //   $('.open-menu-scroll').removeClass('active-menu')
 }
 
 function slowScroll(id) {
-  // id.preventDefault()
-  var offset = 0;
-  // if (id === '.about-service') {
-  //   $('html, body').animate({
-  //     scrollTop: $(id).offset().top - offset - 144
-  //   }, 500);
-  //   removeMenu()
-  // } else if (id === '.footer') {
-  //   $('html, body').animate({
-  //     scrollTop: $(id).offset().top + $(window).height()
-  //   }, 500);
-  //   removeMenu()
-  // } else if (id === '.child-help__title') {
-  //   $('html, body').animate({
-  //     scrollTop: $(id).offset().top - offset - 144
-  //   }, 500);
-  //   removeMenu()
-  // } else 
   if (id === '.header') {
     $('html, body').animate({
       scrollTop: 0
